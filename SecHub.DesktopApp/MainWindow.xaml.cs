48df75a72872f40eec6be579437051a39b534416
Nathan Ferreira <14365254+nathan130200@users.noreply.github.com>
2026-02-25 11:02:31 -0300

    Adicionado projeto inicial do desktop app (ainda estou decidindo se será WPF ou WinForms)

---
+++ SecHub.DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace SecHub.DesktopApp;
+
+public partial class MainWindow : MetroWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnFunaoNaoImplementata_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        this.ShowMessageAsync("⚠ AVISO", "FUNÇÂO NÃO IMPLEMENTADA", MessageDialogStyle.Affirmative);
+    }
+}
